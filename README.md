6a1ad0ca32e9a2c3528eabb8a9aade20c3b54722
Koo <koo@fruit.team>
2020-10-02 15:04:33 +0900

    add tab navigation example

--- README.md
+++ README.md
@@ -33,7 +33,35 @@ expo install react-native-reanimated react-native-gesture-handler react-native-s
 # [react native cli]
 npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
 
-cd ios
-pod install
-cd ..
+# Navigation
+npm install @react-navigation/native
+# [expo cli]
+expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+# [react native cli]
+npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+# [tab navigation] —> https://reactnavigation.org/docs/tab-based-navigation
+npm install @react-navigation/bottom-tabs
+
+# [drawer navigation] —> https://reactnavigation.org/docs/drawer-based-navigation
+npm install @react-navigation/drawer
+
+# Icons —> https://www.npmjs.com/package/react-native-ionicons
+npm install react-native-ionicons
+# ios/Info.plist에 폰트 추가, 폰트 ios/android에 링크해줌
+# react 6.x 에서는 안해도 된다고 함
+npx react-native link react-native-ionicons
+# ios에 폰트 설치해줌
+cd ios && pod install && cd ..
+```
+
+## 라이브러리 변경되면 pod install 해줘야 함
+
+```
+npx pod-install ios
+
+# or
+
+cd ios && pod install && cd ..
 ```
---
+++ navigation5/App.js
@@ -0,0 +1,79 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Icon from 'react-native-ionicons'
+import {NavigationContainer, TabActions} from '@react-navigation/native'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+const App: () => React$Node = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name='Home' component={HomeScreen} options={{ tabBarBadge: 3 }} />
+        <Tab.Screen name='Settings' component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
